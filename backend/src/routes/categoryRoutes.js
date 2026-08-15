510401aef91e7e276905d80499ea5754569fb0fc
Tasnim Binta Monir <tasnimahona00@gmail.com>
2026-08-15 23:36:18 +0600

    Delete backend directory

--- backend/src/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const categoryController = require('../controllers/categoryController');
-
-const router = express.Router();
-
-router.get('/', categoryController.getCategories);
-
-module.exports = router;
--- backend/src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const { authenticate } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post('/', authenticate, reviewController.createReview);
-
-module.exports = router;
